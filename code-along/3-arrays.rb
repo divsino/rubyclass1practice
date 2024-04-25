de916e656c85a964544d236ad4a9bad5a290e84d
Divyansh Singh <divyansh.singh@kellogg.northwestern.edu>
2024-04-25 15:10:00 +0000

    hello

--- code-along/3-arrays.rb
+++ code-along/3-arrays.rb
@@ -3,10 +3,16 @@
 # ruby 3-arrays.rb
 
 # Arrays are lists of things
+favorite_food = ["tacos", "bourbon", "tea"]
 
 # Accessing the array
 
+
 # Add to the array
+favorite_food = favorite_food + ["ice cream"]
+favorite_food.push("ice cream")
+favorite_food << "burgers"
+puts favorite_food.reverse
 
 # There are lots of fun things you can do with arrays:
 # https://ruby-doc.org/core-2.7.0/Array.html
--- labs/3-arrays.rb
+++ labs/3-arrays.rb
@@ -6,10 +6,16 @@
 # Create a "shared" shopping list with a friend
 # Create two data structures - one for your list of stuff, and one
 # for your friend, e.g. you want milk, eggs, and bacon, and
+my_shopping_list = ["milk", "eggs", "bacon"]
 # your friend wants beer, cookies, and apples.
+friend_shopping_list = ["beer", "cookies", "apples"]
 # Programmatically combine the two arrays into a single list,
+combined_list = my_shopping_list + friend_shopping_list
+puts combined_list
 # sort the result (alphabetically), and write it to the screen.
+
 # If the two lists contain the same item, only show it once!
+
 # Lastly, display each item in the list prepended with "buy ".
 
 # HINTS
